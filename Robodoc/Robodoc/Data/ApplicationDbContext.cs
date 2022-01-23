adff3e735b19e94fde4b0d114cab321f4f2ba63b
marco <marco.ebneter@gmail.com>
2022-01-23 16:15:13 +0100

    models created

--- Robodoc/Robodoc/Data/ApplicationDbContext.cs
+++ Robodoc/Robodoc/Data/ApplicationDbContext.cs
@@ -2,16 +2,24 @@
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using Robodoc.Data.Models;
 using Robodoc.Models;
 
 namespace Robodoc.Data
 {
     public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
     {
+        public DbSet<Person> Persons => Set<Person>();
+        public DbSet<Patient> Patients => Set<Patient>();
         public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
             : base(options, operationalStoreOptions)
         {
-
+            
         }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Patient>().HasOne(p => p.Person);
+        //}
     }
 }
