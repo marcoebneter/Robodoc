dfd3320ecb59889d706cea9cf1a4178600e299c2
marcoebneter <marco.ebneter@gmail.com>
2022-05-16 21:12:09 +0200

    refactor

---
+++ robodoc.frontend/robodoc.frontend/Pages/Login.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
+
+namespace robodoc.frontend.Pages
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public string? Username { get; set; }
+        [BindProperty]
+        public string? Password { get; set; }
+        public string? Msg { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (Username == null || Password == null)
+            {
+                Msg = "Invalid";
+                return Page();
+            }
+            else if (Username.Equals("abc") && Password.Equals("123"))
+            {
+                var identity = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, Username)
+                }, CookieAuthenticationDefaults.AuthenticationScheme);
+                var principal = new ClaimsPrincipal(identity);
+                HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+                return RedirectToPage("Welcome");
+            }
+            else
+            {
+                Msg = "Invalid";
+                return Page();
+            }
+        }
+    }
+}
